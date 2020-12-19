d1361b9f833fd152065e462a80d795abd6843100
s18899pjatk <s18899@pjwstk.edu.pl>
2020-12-19 15:55:31 +0100

    configured webpack

--- Assignment10/temp_dist_converter/webpack.config.js
+++ Assignment10/temp_dist_converter/webpack.config.js
@@ -1,16 +1,43 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devServer: {
+    // for react router
     historyApiFallback: true,
   },
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      filename: "index.html",
+      favicon: path.resolve(__dirname, "public", "favicon.ico"),
+    }),
+  ],
 };
